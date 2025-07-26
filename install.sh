b0b4e6601725306b0b226e573c4f902af9c27013
simonsays-techtalk <150742975+simonsays-techtalk@users.noreply.github.com>
2025-07-26 22:17:35 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ WIRETIDE_DIR="/opt/wiretide"
 CERT_DIR="/etc/wiretide/certs"
 DB_FILE="$WIRETIDE_DIR/wiretide.db"
 LOG_FILE="/var/log/wiretide.log"
-REPO_URL="https://github.com/<your-repo>/wiretide-controller.git"
+REPO_URL="https://github.com/simonsays-techtalk/wiretide-controller.git"
 PYTHON_BIN="/usr/bin/python3"
 
 echo "[*] Installing Wiretide Controller..."
