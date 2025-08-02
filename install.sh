d04f0f8464b5f29b90e876322a93bc7089c754d6
simonsays-techtalk <150742975+simonsays-techtalk@users.noreply.github.com>
2025-08-02 22:55:10 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
 set -e
+ 
 
-echo "[*] Installing from branch: $BRANCH"
-if [ "$BRANCH" = "main" ]; then
-    echo "==> PRODUCTION INSTALL"
-else
-    echo "==> DEVELOPMENT INSTALL — beta branch"
-fi
 
 
 WIRETIDE_DIR="/opt/wiretide"
